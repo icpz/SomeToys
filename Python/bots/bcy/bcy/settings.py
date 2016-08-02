e7c377bb15d7e25c024eaa0bbfceef9b49aaf4c9
Paizhuo.Chen <lcdtyph@gmail.com>
2016-08-02 11:02:53 +0800

    remove username & password

--- Python/bots/bcy/bcy/settings.py
+++ Python/bots/bcy/bcy/settings.py
@@ -89,5 +89,3 @@ ITEM_PIPELINES = {
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
-username = 'lcdtyph@gmail.com'
-password = '55363642'
